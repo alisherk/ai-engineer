eb2bf2787ba153352314ed47843a362e9565b171
Alisher Kabildjanov <alisherkabildjanov@yahoo.com>
2026-02-11 22:26:03 +0000

    feat: added simple agent

---
+++ src/app/agents/main.py
@@ -0,0 +1,21 @@
+import asyncio
+
+from agents import Agent, Runner, trace
+
+from src.app.settings import get_settings
+
+settings = get_settings()
+
+agent = Agent(
+    name="Jokster",
+    instructions="Your job is to tell jokes",
+    model=settings.openai_model,
+)
+
+async def main():
+    with trace("Running the agent to tell a joke"):
+        result = await Runner().run(agent, "Tell me a joke about airplanes")
+        print(result.final_output)
+
+
+asyncio.run(main())
