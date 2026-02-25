84916875b3ceaadb45c3d476e8eb501bd7aafef7
Alisher Kabildjanov <alisherkabildjanov@yahoo.com>
2026-02-25 23:49:13 +0000

    feat: got rid of not needed files

--- src/app/agent_workflows/simple_agent_collab.py
+++ src/app/agent_workflows/simple_agent_collab.py
@@ -1,25 +1,14 @@
-from typing import Dict
 import asyncio
+from typing import Dict
 
 from agents.agent import Agent
 from agents.run import Runner
 from agents.tool import function_tool
 from agents.tracing import trace
-from openai.types.responses import ResponseTextDeltaEvent
 
 from src.app.settings import get_settings
 
 settings = get_settings()
-
-@function_tool
-async def send_test_email(email_content: str) -> dict:
-    """A test tool that simulates sending an email."""
-    print("Sending test email...")
-    print(f"Email content:\n{email_content}")
-    await asyncio.sleep(1)
-    print("Test email sent!")
-    return {"status": "success", "message": "Test email sent successfully."}
-
 
 instructions1 = "You are a sales agent working for ComplAI, \
 a company that provides a SaaS tool for ensuring SOC2 compliance and preparing for audits, powered by AI. \
@@ -46,32 +35,6 @@ sales_agent2 = Agent(
 sales_agent3 = Agent(
     name="Busy Sales Agent", instructions=instructions3, model=settings.openai_model
 )
-
-
-async def run_agent_one():
-    result = Runner.run_streamed(sales_agent1, input="Write a cold sales email")
-
-    async for event in result.stream_events():
-        if event.type == "raw_response_event" and isinstance(
-            event.data, ResponseTextDeltaEvent
-        ):
-            print(event.data.delta, end="", flush=True)
-
-
-async def run_agents_in_parallel():
-    message = "Write a cold sales email"
-
-    with trace("Parallel cold emails"):
-        results = await asyncio.gather(
-            Runner.run(sales_agent1, message),
-            Runner.run(sales_agent2, message),
-            Runner.run(sales_agent3, message),
-        )
-
-    outputs = [result.final_output for result in results]
-
-    for output in outputs:
-        print(output + "\n\n")
 
 
 sales_picker = Agent(
@@ -103,13 +66,27 @@ async def run_agent_collaboration():
 
 
 # Part 2 - Use of tools in agents
+
 description = "Write a cold sales email"
+
 # We can convert agents into tools and give them to other agents to use. This allows for more complex interactions and collaborations between agents, where they can call each other as tools to accomplish a task.
 tool1 = sales_agent1.as_tool(tool_name="sales_agent1", tool_description=description)
 tool2 = sales_agent2.as_tool(tool_name="sales_agent2", tool_description=description)
 tool3 = sales_agent3.as_tool(tool_name="sales_agent3", tool_description=description)
 
+
+@function_tool
+async def send_test_email(email_content: str) -> dict:
+    """A test tool that simulates sending an email."""
+    print("Sending test email...")
+    print(f"Email content:\n{email_content}")
+    await asyncio.sleep(1)
+    print("Test email sent!")
+    return {"status": "success", "message": "Test email sent successfully."}
+
+
 tools = [tool1, tool2, tool3, send_test_email]
+
 
 async def run_master_agent_with_tools():
     instructions = """
@@ -143,28 +120,43 @@ async def run_master_agent_with_tools():
 # Handoffs represent a way agent can delegate to another agent, passing control to it
 # Handoff and Agent as tools are similar,
 # With tools, control passes back to the original agent after the tool is done, while with handoffs, control is fully passed to the other agent and does not return
-
 subject_instructions = "You can write a subject for a cold sales email. \
 You are given a message and you need to write a subject for an email that is likely to get a response."
-subject_writer = Agent(name="Email subject writer", instructions=subject_instructions, model=settings.openai_model)
-subject_tool = subject_writer.as_tool(tool_name="subject_writer", tool_description="Write a subject for a cold sales email")
+subject_writer = Agent(
+    name="Email subject writer",
+    instructions=subject_instructions,
+    model=settings.openai_model,
+)
+subject_tool = subject_writer.as_tool(
+    tool_name="subject_writer",
+    tool_description="Write a subject for a cold sales email",
+)
 
 html_instructions = "You can convert a text email body to an HTML email body. \
 You are given a text email body which might have some markdown \
 and you need to convert it to an HTML email body with simple, clear, compelling layout and design."
-html_converter = Agent(name="HTML email body converter", instructions=html_instructions, model=settings.openai_model)
-html_tool = html_converter.as_tool(tool_name="html_converter",tool_description="Convert a text email body to an HTML email body")
+html_converter = Agent(
+    name="HTML email body converter",
+    instructions=html_instructions,
+    model=settings.openai_model,
+)
+html_tool = html_converter.as_tool(
+    tool_name="html_converter",
+    tool_description="Convert a text email body to an HTML email body",
+)
 
 instructions = "You are an email formatter and sender. You receive the body of an email to be sent. \
 You first use the subject_writer tool to write a subject for the email, then use the html_converter tool to convert the body to HTML. \
 Finally, you use the send_html_email tool to send the email with the subject and HTML body."
 
+
 @function_tool
 def send_html_email(subject: str, html_body: str) -> Dict[str, str]:
-    """ Send out an email with the given subject and HTML body to all sales prospects """
+    """Send out an email with the given subject and HTML body to all sales prospects"""
     print("Sending email with subject:", subject)
     print("HTML body:", html_body)
     return {"status": "success"}
+
 
 email_agent_tools = [subject_tool, html_tool, send_html_email]
 emailer_agent = Agent(
@@ -206,8 +198,11 @@ async def run_master_agent_with_handoffs():
     message = "Send out a cold sales email addressed to Dear CEO from Alice"
 
     with trace("Automated SDR"):
-       result = await Runner.run(sales_manager, message)
-       print("Final output from Sales Manager (should be from Email Manager):", result.final_output)
+        result = await Runner.run(sales_manager, message)
+        print(
+            "Final output from Sales Manager (should be from Email Manager):",
+            result.final_output,
+        )
 
 
 asyncio.run(run_master_agent_with_handoffs())
