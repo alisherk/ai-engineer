074f732f2caf807bd142901ccbaeee3452d4e0c1
Alisher Kabildjanov <alisherkabildjanov@yahoo.com>
2026-03-02 23:22:07 +0000

    feat: added stock_picker project

--- src/app/crew_ai/stock_picker/src/stock_picker/crew.py
+++ src/app/crew_ai/stock_picker/src/stock_picker/crew.py
@@ -2,6 +2,9 @@ from typing import List
 
 from crewai import Agent, Crew, Process, Task
 from crewai.agents.agent_builder.base_agent import BaseAgent
+from crewai.memory import EntityMemory, LongTermMemory, ShortTermMemory
+from crewai.memory.storage.ltm_sqlite_storage import LTMSQLiteStorage
+from crewai.memory.storage.rag_storage import RAGStorage
 from crewai.project import CrewBase, agent, crew, task
 from crewai_tools import SerperDevTool
 from pydantic import BaseModel, Field
@@ -48,6 +51,7 @@ class StockPicker:
         return Agent(
             config=self.agents_config["trending_company_finder"],
             verbose=True,
+            memory=True,
         )
 
     @task
@@ -80,6 +84,7 @@ class StockPicker:
             config=self.agents_config["stock_picker"],
             verbose=True,
             tools=[PushNotificationTool()],
+            memory=True,
         )
 
     @task
@@ -96,10 +101,44 @@ class StockPicker:
             verbose=True,
         )
 
+        short_term_memory = ShortTermMemory(
+            storage=RAGStorage(
+                embedder_config={
+                    "provider": "openai",
+                    "config": {
+                        "model": "text-embedding-3-small",
+                    },
+                },
+                type="short_term",
+                path="./memory/",
+            ),
+        )
+
+        long_term_memory = LongTermMemory(
+            storage=LTMSQLiteStorage(db_path="./memory/ltm.db"),
+        )
+
+        entity_memory = EntityMemory(
+            storage=RAGStorage(
+                embedder_config={
+                    "provider": "openai",
+                    "config": {
+                        "model": "text-embedding-3-small",
+                    },
+                },
+                type="short_term",
+                path="./memory/",
+            )
+        )
+
         return Crew(
             agents=self.agents,
             tasks=self.tasks,
             process=Process.hierarchical,  # why not sequential? We want the manager to be able to delegate tasks to the agents as needed, rather than having a fixed order of execution. This allows for more flexibility and adaptability in the crew's workflow.
             verbose=True,
             manager_agent=manager,
+            memory=True,
+            long_term_memory=long_term_memory,
+            short_term_memory=short_term_memory,
+            entity_memory=entity_memory,
         )
